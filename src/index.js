d8d2b7a318710d4b91c681e601f22717ce44cfc5
rzaragoza <ricardo.zaragoza@riac.dev>
2022-10-28 17:01:24 -0500

    First commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const res = require('express/lib/response')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
