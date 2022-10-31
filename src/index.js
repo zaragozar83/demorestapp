bd837c2e7188d79ea6058f07635e2940a07e762b
Ricardo Zaragoza Solis <zaragozar83@gmail.com>
2022-10-31 13:22:03 -0500

    Update https server

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
+const https = require("https")
 const express = require('express')
 const res = require('express/lib/response')
 const app = express()
-const port = 8080
+const port = 3000
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+https
+.createServer(app)
+.listen(8080, ()=>{
+    console.log(`Example app listening on port 8080`)
 })
+
+// app.listen(port, () => {
+//     console.log(`Example app listening on port ${port}`)
+// })
