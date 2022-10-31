32aeda10dcce209eacf05d9e7407707abed38322
Ricardo Zaragoza Solis <zaragozar83@gmail.com>
2022-10-31 13:44:55 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const res = require('express/lib/response')
 const app = express()
-const port = 8080
+const port = 3000
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
