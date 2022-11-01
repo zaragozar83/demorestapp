87335208c904573eab41af66e6e51b85d4be1c5e
rzaragoza <ricardo.zaragoza@riac.dev>
2022-11-01 11:34:03 -0500

    Implement https

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-const express = require('express')
 const res = require('express/lib/response')
+const express = require("express");
+const fs = require("fs")
+const https = require("https")
 const app = express()
-const port = 3000
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+https
+  .createServer(
+    {
+      key: fs.readFileSync("server.key"),
+      cert: fs.readFileSync("server.cert"),
+    },
+    app
+  )
+  .listen(8080, function () {
+    console.log(
+      "Example app listening on port 8080! Go to https://localhost:8080/"
+    );
+  });
